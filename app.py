20426bc91f1a3260723146f7a4ad7e1a674eec37
Nitish Jadia <nitish@jadia.dev>
2021-05-29 03:36:36 +0530

    Fix flake8 errors and warnings

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, url_for, send_file
-from wtforms import Form, TextField, TextAreaField, StringField, SubmitField, validators
+from flask import Flask, render_template, request, send_file
+from wtforms import Form, StringField
 from extract import GetFileLinks
 from datetime import datetime as dt
 import shortuuid
@@ -7,15 +7,18 @@ import os
 
 
 URL_TXT_DIR = 'files'
-if not os.path.exists(URL_TXT_DIR): os.makedirs(URL_TXT_DIR)
+if not os.path.exists(URL_TXT_DIR):
+    os.makedirs(URL_TXT_DIR)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
 APP_NAME = 'Link Extractor'
 
+
 class InputForm(Form):
     input_url = StringField('Directory URL')
     extension = StringField('Extension of files')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -28,25 +31,27 @@ def index():
         url_list = file_links_obj.get_file_links()
 
         unique_id = shortuuid.uuid(name=url)
-        url_list_file_name = url.split("://")[1].split("/")[0] + '-' + unique_id + '.txt'
-        url_list_file_path = os.path.join(URL_TXT_DIR ,url_list_file_name)
+        url_list_file_name = '-'.join([url.split("://")[1].split("/")[0],
+                                      unique_id])
+        url_list_file_name = '.'.join([url_list_file_name, 'txt'])
+        print(url_list_file_name)
+        url_list_file_path = os.path.join(URL_TXT_DIR, url_list_file_name)
         with open(url_list_file_path, 'w') as f:
             f.writelines("%s\n" % url for url in url_list)
 
-        return render_template('result.html', 
-                                extension=ext, 
-                                result=url_list,
-                                file_name=url_list_file_name)
+        return render_template('result.html', extension=ext, result=url_list,
+                               file_name=url_list_file_name)
     else:
         print("Get request.")
         return render_template('index.html', form=form)
 
+
 @app.route('/download/<file_name>', methods=['GET'])
 def download_url_file(file_name):
-    # unique_id = shortuuid.uuid(name=url)
-    # url_list_file = url.split("://")[1].split("/")[0] + '-' + unique_id + '.txt'
-    url_list_file = os.path.join(URL_TXT_DIR ,file_name)
-    return send_file(url_list_file, as_attachment=True, attachment_filename='urls_list.txt')
+    url_list_file = os.path.join(URL_TXT_DIR, file_name)
+    return send_file(url_list_file, as_attachment=True,
+                     attachment_filename='urls_list.txt')
+
 
 @app.context_processor
 def inject_now():
@@ -58,9 +63,10 @@ def inject_app_name():
     return {'app_name': APP_NAME}
 
 
-@app.errorhandler(404) 
-def not_found(e): 
-    return render_template("404.html") 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000)
+    app.run(host='0.0.0.0', port=80)
--- extract.py
+++ extract.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3.8
-import sys
 from typing import List
 from bs4 import BeautifulSoup
 import requests
@@ -7,19 +6,21 @@ import argparse
 
 
 class GetFileLinks():
-    """Get direct links of all the files with a specific extension on the HTML page.
+    """Get direct links of all the files with a specific extension on the HTML
+    page.
     """
     def __init__(self, url, ext) -> None:
         self.url = url
         self.ext = ext
 
     def get_file_links(self) -> List:
-        """Extract links from webpage and return list of URL with the provided extension.
+        """Extract links from webpage and return list of URL with the provided
+        extension.
 
         Args:
             url ([str]): URL to a webpage
-            ext ([str]): extension of files whose direct links need to be extracted
-                            from the provided URL
+            ext ([str]): extension of files whose direct links need to be
+            extracted from the provided URL
 
         Returns:
             List: List of URL with extension
@@ -27,22 +28,20 @@ class GetFileLinks():
         try:
             page = requests.get(self.url).text
             soup = BeautifulSoup(page, 'html.parser')
-            #Adapted from: https://stackoverflow.com/a/34718858
-            return [self.url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(self.ext)]
+            # Adapted from: https://stackoverflow.com/a/34718858
+            return [self.url + node.get('href') for node in soup.find_all('a')
+                    if node.get('href').endswith(self.ext)]
         except Exception as e:
+            print(e)
             return ['Something went wrong! Please check the input.']
 
 
 if __name__ == '__main__':
-    url_args = argparse.ArgumentParser(description='Extract urls from index of/ page')
-    url_args.add_argument('--url',
-                            help='Index of/ page URL', 
-                            required=True,
-                            type=str)
-    url_args.add_argument('--ext',
-                            help='Extension of the file URL to extract', 
-                            required=True,
-                            type=str)
+    url_args = argparse.ArgumentParser(description='Extract urls from webpage')
+    url_args.add_argument('--url', help='Index of/ page URL', required=True,
+                          type=str)
+    url_args.add_argument('--ext', help='Extension of the file URL to extract',
+                          required=True, type=str)
 
     args = url_args.parse_args()
     url = args.url
